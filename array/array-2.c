fb08e1c6b156d1d4f75cc8e01b6a8910457f1173
kjhwan <kjhwan1316@gmail.com>
2026-03-27 15:05:40 +0900

    C언어 실

---
+++ array/array-2.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define STUDENTS 5 //기호상수
+
+int main(void) {
+
+	int score[STUDENTS]; //5개
+	int sum = 0;
+	double divi;
+	int cnt = 0;
+		for (int i = 0; i < 5; i++) {
+			while (1) { //올바른 값을 입력받릉 때까지 반복
+				printf("학생의 성적을 입력해주세요: ");
+				scanf("%d", &score[i]);
+				if (score[i] >= 0 && score[i] <= 100) {
+					sum = sum + score[i];
+					cnt += 1;
+					break;
+				}
+				else {
+					printf("다시 입력해주세요\n");
+				}
+			}
+		}
+
+		divi = (double) sum / cnt;
+		printf("합계: %d, 평균: %.2f", sum, divi);
+
+
+
+
+	return 0;
+}
