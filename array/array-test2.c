c2ff8bdb1953cf840026e43b1bb2da48c7a48c85
kjhwan <kjhwan1316@gmail.com>
2026-03-30 15:46:01 +0900

    배열

---
+++ array/array-test2.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main() {
+
+	char s[] = "ABCD";
+	int a = 0, b = 3;
+	char t;
+	while (a < b) { //0,3번째 교환 -> 1,2번째 교환
+		t = s[a];
+		s[a] = s[b];
+		s[b] = t;
+		a++;
+		b--;
+	}
+
+	printf("%s", s);
+	return 0;
+}
