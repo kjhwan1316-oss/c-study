6e29dfd058b2c5c95ce0b49c6b23a0236bed2d5d
kjhwan <kjhwan1316@gmail.com>
2026-04-01 15:15:18 +0900

    문제 5

---
+++ self-learning/switch_case.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+	int a = 4;
+	switch (a) {
+
+	case 5:
+		printf("A");
+	case 3:
+		printf("B");
+	case 1:
+		printf("c");
+	default:
+		printf("D");
+
+
+
+}
+	return 0;
+
+}
