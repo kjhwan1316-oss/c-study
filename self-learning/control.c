961ba9d88bb47471d74186e2f57bce039d21b914
kjhwan <kjhwan1316@gmail.com>
2026-04-01 15:15:08 +0900

    문제 4

---
+++ self-learning/control.c
@@ -0,0 +1,16 @@
+﻿#include<stdio.h>
+int main() {
+
+
+	int cnt = 0;//정수형 변수 선언
+	for (int i = 1; ;i++) { //정수형 변수 i = 0; ;i에 1씩 더하기
+		if (i > 10){
+			break;
+		}
+		if (i % 3 == 0) {
+			cnt++;
+		}
+	}
+	printf("3의 배수의 개수: %d\n", cnt);
+	return 0;
+}
